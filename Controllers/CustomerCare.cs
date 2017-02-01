328287f7af0358865d93d9c73337b677ddf49932
Colin O'Brien <Colin O'Brien>
2017-02-01 18:46:26 -0500

    Routed CustomerCare to Claims and added fileclaim & claimstatus to my url lines

--- Controllers/CustomerCare.cs
+++ Controllers/CustomerCare.cs
@@ -1,16 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
-
+ 
 public class CustomerCareController : Controller
 
 {
+    [Route("customer")]
     public IActionResult Index()
     {
         return Ok("This is the index of the CustomerCareController");
     }
-    [Route("CustomerCare/Claims")] 
-    public IActionResult quote() 
+    [Route("customer/claims")] 
+    public IActionResult Quote() 
     {
-        return Ok("This is customercare/claims");
+        return Ok("This is customer/claims");
+    }
+    [Route("customer/fileclaim")]
+        public IActionResult NewClaim() 
+    {
+        return Ok("This is customer/fileclaim");
     }
     
+    [Route("customer/claimstatus")]
+        public IActionResult ClaimHistory() 
+    {
+        return Ok("This is claim/claimstatus");
+    }
+
 }
